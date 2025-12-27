5f7c32daa7785bd3d191726654542eb5b7bd0dbc
Srinanth <srinanthmv845@gmail.com>
2025-12-27 23:32:29 +0530

    experiments 3-6

---
+++ exp5 /exp5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid= fork();
+
+    if (pid < 0) {
+        perror("Fork failed");
+        return 1;
+    }
+    else if (pid == 0) {
+        printf("CHILD (PID %d).\n", getpid());
+
+        char *args[] = {"./add", "10", "20", NULL};
+        execvp(args[0], args);
+
+        perror("execvp failed");
+        exit(1);
+    }
+    else {
+        printf("PARENT PID %d.\n", pid);
+        
+        wait(NULL);
+        
+        printf("Child finished execution.\n");
+    }
+
+    return 0;
+}
